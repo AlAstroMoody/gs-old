c9ad896eb1b489026c7e2f53dff07c61f852fd57
AlAstroMoody <aastrotenko@mail.ru>
2023-11-12 16:47:20 +0700

    фикс

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gs/',
+  base: '/gs-old/',
   plugins: [vue()],
   resolve: {
     alias: {
@@ -22,9 +22,7 @@ export default defineConfig({
     },
   },
   optimizeDeps: {
-    include: [
-      'tailwind.config.js',
-    ]
+    include: ['tailwind.config.js'],
   },
   build: {
     commonjsOptions: {
